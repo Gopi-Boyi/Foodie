99fe3df0f0a0729da2b837eb954dfd841a943e84
Gopi Boyi <gopiboyi88@gmail.com>
2026-07-18 17:25:40 +0530

    working on FoodDisplay

--- foodies/src/assets/assets.js
+++ foodies/src/assets/assets.js
@@ -1,7 +1,46 @@
 import logo10 from './logo10.png';
 import cart10 from './cart10.png';
 
+import briyani from './briyani.png';
+import burger from './burger.png';
+import cake from './cake.png';
+import icecream from './icecream.png';
+import pizza from './pizza.png';
+import rools from './rools.png';
+import salad from './salad.png';
+
 export const assets = {
     logo10,
     cart10
 }
+
+export const categories = [
+    {
+        category: 'Briyani',
+        icons: briyani
+    },
+    {
+        category: 'Burger',
+        icons: burger
+    },
+    {
+        category: 'Cakes',
+        icons: cake
+    },
+    {
+        category: 'Ice Creams',
+        icons: icecream
+    },
+    {
+        category: 'Pizza',
+        icons: pizza
+    },
+    {
+        category: 'Rools',
+        icons: rools
+    },
+    {
+        category: 'Salad',
+        icons: salad
+    },
+]
