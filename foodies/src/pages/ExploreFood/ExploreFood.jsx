c45371f50a7dbe7a450a2702505ab6f956e0b0fa
Gopi Boyi <gopiboyi88@gmail.com>
2026-07-21 15:00:50 +0530

    fixed the search bar

--- foodies/src/pages/ExploreFood/ExploreFood.jsx
+++ foodies/src/pages/ExploreFood/ExploreFood.jsx
@@ -1,37 +1,38 @@
 import React from 'react';
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
 
-
 const ExploreFood = () => {
   return (
-    
     <>
       <div className="container">
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-          <form>
-            <div className="imput-group mb-3">
-              <select className='form-select mt-2' style={{'maxWidth': '150px'}}>
-                <option value="Briyani">Briyani</option>
-                <option value="Burger">Burger</option>
-                <option value="Cakes">Cakes</option>
-                <option value="Ice Creams">Ice Creams</option>
-                <option value="Pizza">Pizza</option>
-                <option value="Rools">Rools</option>
-                <option value="Salad">Salad</option>
-              </select>
-              <input type="text" className='form-control mt-2' placeholder="Search your favorite dish....."/>
-              <button className='btn btn-primary mt-2' type='submit'>
-                <i className='bi bi-search'></i>
-              </button>
-            </div>
-          </form>
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <form>
+              <div className="d-flex gap-2 mb-3">
+                <select className='form-select' style={{ maxWidth: '150px' }}>
+                  <option value="Briyani">Briyani</option>
+                  <option value="Burger">Burger</option>
+                  <option value="Cakes">Cakes</option>
+                  <option value="Ice Creams">Ice Creams</option>
+                  <option value="Pizza">Pizza</option>
+                  <option value="Rools">Rools</option>
+                  <option value="Salad">Salad</option>
+                </select>
+                <input
+                  type="text"
+                  className='form-control'
+                  placeholder="Search your favorite dish....."
+                />
+                <button className='btn btn-primary' type='submit'>
+                  <i className='bi bi-search'></i>
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-    <FoodDisplay />
+      <FoodDisplay />
     </>
-    
   )
 }
 
